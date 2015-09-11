9bc4257a5b6d37f649ba4b6a66f3c49b69b8ae63
Alex <alexg@skilstak.sh>
2015-09-11 09:34:41 -0400

    save

--- story/revive.py
+++
@@ -1,7 +0,0 @@
-¨¨¨Revive
-
-    You wake up at the bottom of the mineshaft. You thought that you died, but it seems that you
-have some kind of special power that allows you to come back to life. You don't know 
-the full extent of the power, but you don't want to test it again, in case it was a one-time
-thing.
-¨¨¨
