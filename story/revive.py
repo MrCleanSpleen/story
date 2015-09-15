fd42abd88693cf3074ca4f99ce6a319fba683af0
Alex <alexg@skilstak.sh>
2015-09-14 20:29:34 -0400

    save

--- story/revive.py
+++ story/revive.py
@@ -2,4 +2,5 @@
 
     You wake up at the bottom of the deep hole. It seems as if you have died and 
 come back to life, but you don't know if it will happen again. You don't want to risk 
-it. The walls have torches placed on them at regular intervals. It looks like their
+it. The walls have torches placed on them at regular intervals. It looks like there
+is a bend in the 
