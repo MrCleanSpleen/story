a9e19c01b96ac097c3cfe3148513a1b54068cdf9
Alexander Green <aeg0322@gmail.com>
2015-09-18 09:01:39 -0400

    Update revive.py

--- story/revive.py
+++ story/revive.py
@@ -3,4 +3,6 @@
     You wake up at the bottom of the deep hole. It seems as if you have died and 
 come back to life, but you don't know if it will happen again. You don't want to risk 
 it. The walls have torches placed on them at regular intervals. It looks like there
-is a bend in the 
+is a bend in the tunnel, and you can´t see what is down it. Your gut tells you to go 
+up the ladder to go back to the surface, but you are curious to see what is down the tunnel.
+"""
