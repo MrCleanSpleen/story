41634f2f73a7f93deb690d5e1e0148d02ef7c981
Alex <alexg@skilstak.sh>
2015-09-22 09:32:37 -0400

    save

--- story/ladder.py
+++ story/ladder.py
@@ -2,4 +2,8 @@
 
     You start to climb up the ladder when you see a bright light at the top of
 the mineshaft. You are wondering why it is bright outside, considering it was
-
+the middle of the night when you fell down the hole. You decide to keep climbing 
+up the ladder and finaly come to the top. When you finish climbing out of the 
+hole, you find yourself in a huge, white room as big as a football field and 10
+meters high. At the end of the room are two doors. One of them 
+"""
