cae5bed6722b98194fa7b84de97528bb122970b5
Alexander Green <aeg0322@gmail.com>
2015-11-05 14:30:59 -0500

    Update ladder.py

--- story/ladder.py
+++ story/ladder.py
@@ -5,5 +5,7 @@ the mineshaft. You are wondering why it is bright outside, considering it was
 the middle of the night when you fell down the hole. You decide to keep climbing 
 up the ladder and finaly come to the top. When you finish climbing out of the 
 hole, you find yourself in a huge, white room as big as a football field and 10
-meters high. At the end of the room are two doors. One of them 
+meters high. At the end of the room are two doors. One of them is written in a weird, 
+alien alphabet. The other has the words, "Backup Generator," on it.
 """
+prompt("Do you go into the door with the alien languageon it, or into the backup generator room?")
